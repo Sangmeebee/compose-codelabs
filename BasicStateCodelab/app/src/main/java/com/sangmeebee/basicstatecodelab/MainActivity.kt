f4f2a78c4811714c016d6f50ae22bf4807f221df
Sangmeebee <apfhdznzl@gmail.com>
2023-11-02 10:39:29 +0900

    step3- state

--- BasicStateCodelab/app/src/main/java/com/sangmeebee/basicstatecodelab/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/sangmeebee/basicstatecodelab/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sangmeebee.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicStateCodelabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WellnessScreen()
                 }
             }
         }
@@ -27,17 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCount(modifier)
+}
+
+@Composable
+fun WaterCount(modifier: Modifier = Modifier) {
+    val count = 0
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WellnessScreenPreview() {
     BasicStateCodelabTheme {
-        Greeting("Android")
+        WellnessScreen()
     }
 }
