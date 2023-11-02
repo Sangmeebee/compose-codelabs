4b56c02fb973041771c25611c8def06b117b86e4
Sangmeebee <apfhdznzl@gmail.com>
2023-11-02 11:00:28 +0900

    step6 - state drvien ui

--- BasicStateCodelab/app/src/main/java/com/sangmeebee/basicstatecodelab/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/sangmeebee/basicstatecodelab/MainActivity.kt
@@ -48,12 +48,17 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         modifier = modifier.padding(16.dp)
     ) {
         var count by rememberSaveable { mutableStateOf(0) }
-        Text(text = "You've had $count glasses.")
+
+        if (count > 0) {
+            Text(text = "You've had $count glasses.")
+        }
+
         Button(
             modifier = Modifier.padding(top = 8.dp),
             onClick = {
                 count++
-            }
+            },
+            enabled = count < 10
         ) {
             Text("Add one")
         }
