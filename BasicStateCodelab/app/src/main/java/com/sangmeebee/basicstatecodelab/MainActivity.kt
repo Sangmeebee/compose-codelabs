cd5d82b6b346a356618b0ce080fff6e932e3b708
Sangmeebee <apfhdznzl@gmail.com>
2023-11-02 10:52:31 +0900

    step5- memory in composable function

--- BasicStateCodelab/app/src/main/java/com/sangmeebee/basicstatecodelab/MainActivity.kt
+++ BasicStateCodelab/app/src/main/java/com/sangmeebee/basicstatecodelab/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +47,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        var count = 0
+        var count by rememberSaveable { mutableStateOf(0) }
         Text(text = "You've had $count glasses.")
         Button(
             modifier = Modifier.padding(top = 8.dp),
