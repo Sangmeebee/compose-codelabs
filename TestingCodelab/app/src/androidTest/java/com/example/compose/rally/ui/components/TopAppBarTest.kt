58d83853905cbdd61313c69fe041d7dfba10ee69
Sangmeebee <apfhdznzl@gmail.com>
2023-12-14 11:09:01 +0900

    create a simple UI Test

---
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/ui/components/TopAppBarTest.kt
@@ -0,0 +1,28 @@
+package com.example.compose.rally.ui.components
+
+import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import com.example.compose.rally.RallyScreen
+import com.example.compose.rally.ui.theme.RallyTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TopAppBarTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun rallyTopAppBar() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTheme {
+                RallyTopAppBar(allScreens = allScreens, onTabSelected = {}, currentScreen = RallyScreen.Accounts)
+            }
+        }
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertIsSelected()
+    }
+}
